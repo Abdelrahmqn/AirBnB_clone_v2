50c729a668a990733bd93408f8c9ba68b4dddae4
A <boda8099@gmail.com>
2024-05-19 23:26:02 +0300

    1-hbnb_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Flask Documentation file """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def its_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<element>', strict_slashes=False)
+def print_text(element):
+    return f'C {element}'
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True, host="0.0.0.0")
